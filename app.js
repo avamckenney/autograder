c746df98877c5be572f868e8a81740a132d980c1
Ava McKenney <avamckenney@cunet.carleton.ca>
2025-08-30 15:41:37 -0400

    wgewgrgw

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.use("/", function(req, res, next) {
     //console.log("User is not authenticated, redirecting to login");
     logger.warn("User is not authenticated, redirecting to login");
     
-    if(!req.session.returnTo && req.path !== "/login.html" && req.path !== "/login" && req.path !== "favicon.ico") {
+    if(!req.session.returnTo && req.path.startsWith("/users/")) {
       req.session.returnTo = req.protocol + '://' + req.host + req.url; // Store the original URL in the session
       logger.info("Setting return to for session: " + req.session.returnTo);
     }
